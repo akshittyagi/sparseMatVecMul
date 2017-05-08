a48f18c0107b9f7404d1e69a738894e4fe07890b
Akshit Tyagi <akty.tyagi7@gmail.com>
2017-05-08 18:09:42 +0530

    Update building scripts

--- compile.sh
+++ compile.sh
@@ -1,3 +1,4 @@
+#To compile on the HPC provided at IIT Delhi
 module load compiler/cuda/7.5/compilervars
 module load compiler/gcc/4.9.3/compilervars
 module load mpi/mpich/3.1.4/gcc/mpivars
--- run.sh
+++ run.sh
@@ -1,1 +1,2 @@
+# Arg1 is the input file name with the matrix in COO formatand Arg2 is the output file 
 time mpirun -np 8 ./main $1 $2
